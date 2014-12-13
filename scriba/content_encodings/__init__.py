f3194095df43fb3537a55945e0b944d9a9effab3
Luca De Vitis <luca@monkeython.com>
2014-12-13 12:30:05 +0100

    File rename slight api change

--- scriba/content_encodings/__init__.py
+++ scriba/content_encodings/__init__.py
@@ -1,6 +1,1 @@
-from cereal.content_encodings import bzip2
-from cereal.content_encodings import compress
-from cereal.content_encodings import gzip_
-
-
-__all__ = ['bzip2', 'compress', 'gzip_']
+__import__('pkg_resources').declare_namespace(__name__)
